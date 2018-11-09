38559f1cdda93675dcd8f9a6560e8ef7b62defe3
Sam Swerdling <you@example.com>
2018-11-08 20:08:52 -0700

    Current

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,20 @@
+
+
+
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
 		 var wins = 0;
 		 var losses = 0;
 		 var numGuesses = 9;
-		 var guessChoices = [];
+		 var guessChoice = [];
 
 		 document.onkeyup = function(event) {
 
-		 	var userGuess = event.key;
+             var userGuess = event.key.toLowerCase();
 
 		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            
+             console.log(computerGuess);
 
 		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 		 	
@@ -19,19 +24,21 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 				if (userGuess === computerGuess) {
 					wins++;
 					numGuesses = 9;
-					guessChoices = [];
+                    guessChoice = [];
+                    document.querySelector(".Winning-key").innerHTML = "your right the answer was: " + computerGuess;
+                    
 				}
 
 				if (userGuess != computerGuess) {
 					numGuesses --;
-					guessChoices.push(userGuess);
+					guessChoice.push(userGuess);
 				}
 
 				if (numGuesses === 0) {
 
 				numGuesses = 9;
 				losses ++;
-				guessChoices = [];
+				guessChoice = [];
 
 				
 			}
@@ -42,7 +49,9 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 			"<p>Wins: " + wins + "</p>" +
 			"<p>Losses: " + losses + "</p>" +
 			"<p>Guesses Left: " + numGuesses + "</p>" +
-			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+            "<p>Your Guesses so far: " + guessChoice.join(", ") + "</p>"
+            "<p class='Winning-key'></p>"
+            ;
 
 			document.querySelector("#game").innerHTML = html;
 
